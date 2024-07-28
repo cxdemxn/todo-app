aa38878c73eb479d540d5d011dea886d978f5f10
barizaa <donbarizaa@gmail.com>
2024-07-28 14:26:25 +0100

    working on main view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './styles/style.css'
 import './styles/menu.css'
+import './styles/list.css'
 import Task from './components/Task'
 import pageLoad from './utils/pageLoad'
 
 const newTask = new Task('title', 'some odd description', new Date());
 
 document.addEventListener('DOMContentLoaded', () => {
-    pageLoad();
+    // pageLoad();
+});
+
+document.querySelector('#testTask').addEventListener('click', () => {
+    console.log('btn or bts')
+});
+
+document.querySelector('.taskCheck').addEventListener('click', event => {
+    event.stopPropagation();
+    console.log('check or cheek');
 });
